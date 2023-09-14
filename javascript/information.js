8c9596b8b6900685cb9bccd88ec110095c4e09f9
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-09-14 22:36:56 +0530

    project_work_add

--- javascript/information.js
+++ javascript/information.js
@@ -1,15 +1,23 @@
 fetch("/json/information.json")
 .then((res)=>res.json())
 .then((data)=>{
-    const div=document.getElementsByClassName("img-fluid rounded-start");
-    // console.log("div",div)
+  console.log(data);
+    const div=document.getElementById("cardImg");
+    console.log("div",div)
     data.forEach((element) => {
         const img = document.createElement("div");
-        img.innerHTML = ` <img
-        src="${element.img}"
+        img.innerHTML = `
+        <img
+        src="${element.image}"
         class="img-fluid rounded-start"
         alt="..."
-      />`;
+       
+      />
+      <div class="col-md-8">
+      <h3>${element.title}</h3>
+      <p>${element.information}</p>
+      `;
+      
       div.appendChild(img);
       });
 
