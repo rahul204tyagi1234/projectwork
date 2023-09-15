51bed953f1c6db648608cf6741da15df2375f25d
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-09-15 23:41:07 +0530

    add word today

--- javascript/information.js
+++ javascript/information.js
@@ -1,24 +1,42 @@
 fetch("/json/information.json")
-.then((res)=>res.json())
-.then((data)=>{
-  console.log(data);
-    const div=document.getElementById("cardImg");
-    console.log("div",div)
+  .then((res) => res.json())
+  .then((data) => {
+    const div = document.getElementById("cardImg");
+
     data.forEach((element) => {
-        const img = document.createElement("div");
-        img.innerHTML = `
+      const img = document.createElement("div");
+      img.classList.add("row", "g-0");
+      img.innerHTML = `
+         <div class="col-md-4">
         <img
         src="${element.image}"
         class="img-fluid rounded-start"
         alt="..."
-       
       />
+      </div>
       <div class="col-md-8">
+      <div class="card-body">
       <h3>${element.title}</h3>
       <p>${element.information}</p>
+      </div>
+      </div>
       `;
-      
-      div.appendChild(img);
-      });
+      //   const img2 = document.createElement("div");
+      //   img2.innerHTML = `
 
-});
+      // <div class="col-md-8">
+      // <h3>${element.title}</h3>
+      // <p>${element.information}</p></div>
+      // <div class="col-md-4">
+      //   <img
+      //   src="${element.image}"
+      //   class="img-fluid rounded-start"
+      //   alt="..."
+
+      // /></div>
+      // `;
+
+      div.appendChild(img);
+      // div.appendChild(img2);
+    });
+  });
