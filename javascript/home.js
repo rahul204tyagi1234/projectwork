12463677e5ed88e3b435dc1baa5d9fc120dde904
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-09-24 20:42:33 +0530

    add-work

---
+++ javascript/home.js
@@ -0,0 +1,8 @@
+const home = document.getElementById("all-pages");
+
+// fatch("")
+// .then((res) =>res.text())
+// .then((data)=>{
+//     console.log(data);
+//     home.innerHTML=data;
+// });
--- javascript/login.js
+++ javascript/login.js
@@ -20,4 +20,27 @@
 //     .then((data) => {
 //       showLoginPage.innerHTML = data;
 //     });
-// }
+//
+
+
+function loginDatas(){
+    var email =document.getElementById("emailfeilds").value;
+    var password=document.getElementById("passwordfeilds").value;
+    var loginEmail=document.getElementById("emailfeild").value;
+    var loginPassword=document.getElementById("passwordfeild").value;
+
+    if(email===loginEmail && password===loginPassword){
+        document.getElementById("")
+    }
+    else if(email!=loginEmail){
+        alert("your email is not matching");
+    }
+    else if(password!=loginPassword){
+        alert("your password is not matching");
+    }
+    else{
+        alert("you can't go at dasboard");
+    }
+
+
+}
